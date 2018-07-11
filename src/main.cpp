dddd96aabc9df80710c96684cf910b3a0234a930
DKuch <c.DamianKucharski@gmail.com>
2018-07-11 18:58:27 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,12 @@
 
 void setup() {
     pinMode(13,OUTPUT);
-<<<<<<< HEAD
-    
-||||||| merged common ancestors
-=======
     Servo myServo;
     myServo.attach(10);
->>>>>>> 3f4120d3835a90ca418da306d5e6441cb085765b
 }
 
 void loop() {
     
-<<<<<<< HEAD
     
+}
 
-}
-||||||| merged common ancestors
-}
-=======
-}
->>>>>>> 3f4120d3835a90ca418da306d5e6441cb085765b
