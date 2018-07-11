450c428ffd0fa1f9c148b929364d2e0d30daf366
Damian <c.damiankucharski@gmail.com>
2018-07-11 20:19:40 +0200

    super

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,30 @@
 #include <Arduino.h>
 #include <Servo.h>
+#include <math.h>
+
+
+int val = 0;
+Servo myServo;
+int angle;
 
 void setup() {
-    pinMode(13,OUTPUT);
-    Servo myServo;
+
+   // pinMode(13,OUTPUT);
     myServo.attach(10);
+    Serial.begin(9600);
+    pinMode(A0,INPUT);
+    myServo.write(90);
 }
 
 void loop() {
-    
-    
-}
 
+    val = analogRead(A0);
+    angle = abs(val*180/1023);
+    
+    myServo.write(angle);
+    Serial.println(angle);
+    
+    delay(50);
+
+
+}
