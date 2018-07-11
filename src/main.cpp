5af240ea555c43a077d84bb9a00b286864d9b095
Damian <c.damiankucharski@gmail.com>
2018-07-11 22:59:32 +0200

    working simple version

--- src/main.cpp
+++ src/main.cpp
@@ -11,37 +11,7 @@ int valstart = 0;
 int difference = 0;
 int speed = 0;
 
-int getLowerDifference()
-{
 
-    if(valstart > 511)
-        return 1023-valstart;
-    else
-        return valstart;
-
-}
-
-void getDirection(int value)
-{
-
-    if(value < valstart + 10 && value > valstart - 10)
-        direction = 0;
-    else if(value > valstart + 10)
-        direction = 1;
-    else if(value < valstart - 10)
-        direction = -1;
-
-}
-
-int giveMappedValueSpeed(int value)
-{
-    if(valstart > 511)
-        return map(value,valstart-difference,valstart,0,255);
-    else if(valstart <= 511)
-        return map(value,0,valstart,0,255);if(value > valstart + 10)
-
-    return 0;
-}
 
 
 // 3,4 - engines, 6 - pwl for engines
@@ -80,21 +50,37 @@ void setup() {
     digitalWrite(4, LOW);
     pinMode(6,OUTPUT); // pwl engine
     pinMode(10,OUTPUT); // serwo
-    //myServo.attach(10);
+ 
     Serial.begin(9600);
     pinMode(A0,INPUT);
-    //myServo.write(90);
-    valstart = 533;//analogRead(A0);
-    difference = getLowerDifference();
+
+    speed=200;
 }
 
 void loop() {
 
     
     val = analogRead(A0);
-    speed = giveMappedValueSpeed(val);
-    getDirection(val);
-    move(direction,speed);
+    
+    if(val>550) 
+    {
+         digitalWrite(3, LOW);
+            digitalWrite(4, HIGH);
+            analogWrite(6, speed);
+    }
+    else if(val<500) {
+        
+         digitalWrite(4, LOW);
+            digitalWrite(3, HIGH);
+            analogWrite(6, speed);
+    }
+    else if (val>500 && val<550) {
+         digitalWrite(3, LOW);
+            digitalWrite(4, 0);
+            analogWrite(6, 0);
+    }
+
+   // move(direction,speed);
     Serial.println(val);
     
     delay(50);
